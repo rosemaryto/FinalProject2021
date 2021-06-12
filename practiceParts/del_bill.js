7a958461bb74ef46775a255ac96bf2e1ad2d2e33
rosemaryto <rosemary_to@ymail.com>
2021-06-11 21:34:48 -0700

    CCvalidation

--- practiceParts/del_bill.js
+++ practiceParts/del_bill.js
@@ -1,4 +1,3 @@
-//IF OTHER ADDRESS IS SELECTED
 //MAKE TEXT BOX APPEAR
 const otherAddr = document.querySelector('#addressType');
 otherAddr.addEventListener('change', (e) => {
@@ -37,22 +36,36 @@ window.addEventListener('load', () => {
       $('saveSubmit').addEventListener('click', () => {
       //clear error msg
       $('spanName').value = "";
+      $('spanName2').value = "";
       $('spanAddress').value = "";
+      $('spanAddress2').value = "";
       $('spanCity').value = "";
+      $('spanCity2').value = "";
       $('spanState').value = "";
+      $('spanState2').value = "";
       $('spanZip').value = "";
+      $('spanZip2').value = "";
       $('spanPhone').value = "";
       $('spanEmail').value = "";
+      $('spanCVC').value = "";
+      $('spanCC').value = "";
 
       //get values entered by user
       const name = $('name1').value; 
+      const name2 = $('name2').value; 
       const addressType = $('addressType').value;
       const address = $('address1').value;
+      const address2 = $('address2').value;
       const city = $('city1').value;
+      const city2 = $('city2').value;
       const state = $('state1').value;
+      const state2 = $('state2').value;
       const zip = $('zip1').value;
+      const zip2 = $('zip2').value;
       const phone = $('phone').value;
       const email = $('email').value;
+      const cvc = $('cvc').value;
+      const cc = $('creditCard').value;
 
       //regular expressions for validity testing
       const namePattern = /^[a-zA-Z]+(?:\s[a-zA-Z]+)+$/;
@@ -62,6 +75,11 @@ window.addEventListener('load', () => {
       const zipPattern = /^\d{5}?$/;
       const phonePattern = /^\d{3}-\d{3}-\d{4}$/;
       const emailPattern = /^[\w\.\-]+@[\w\.\-]+\.[a-zA-Z]+$/;
+      const cvcPattern = /^\d{3}?$/;
+      const amexPattern = /^(?:3[7][0-9]{13})$/;
+      const visaPattern = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/;
+      const masterPattern = /^(?:5[1-5][0-9]{14})$/;
+      
 
       //check user entries for validity
       let isValid = true;
@@ -71,6 +89,13 @@ window.addEventListener('load', () => {
           $('spanName').innerHTML = "Full Name Required";
       } else {
           $('spanName').innerHTML = "";
+      }
+      //NAME2
+      if (name2 === "" || !namePattern.test(name2)) {
+          isValid = false;
+          $('spanName2').innerHTML = "Full Name Required";
+      } else {
+          $('spanName2').innerHTML = "";
       }
       //ADDRESS TYPE
       if (addressType === "") {
@@ -86,35 +111,63 @@ window.addEventListener('load', () => {
       } else {
           $('spanAddress').innerHTML = "";
       }
-      // //CITY
+      //ADDRESS2
+      if (address2 === "" || !addressPattern.test(address)) {
+          isValid = false;
+          $('spanAddress2').innerHTML = "Street Address Required";
+      } else {
+          $('spanAddress2').innerHTML = "";
+      }
+      //CITY
       if (city === "" || !cityPattern.test(city)) {
           isValid = false;
           $('spanCity').innerHTML = "City Required";
       } else {
           $('spanCity').innerHTML = "";
       }
-      // //STATE
+      //CITY2
+      if (city2 === "" || !cityPattern.test(city)) {
+          isValid = false;
+          $('spanCity2').innerHTML = "City Required";
+      } else {
+          $('spanCity2').innerHTML = "";
+      }
+      //STATE
       if (state === "" || !statePattern.test(state)) {
           isValid = false;
           $('spanState').innerHTML = "State Required";
       } else {
           $('spanState').innerHTML = "";
       }
-      // //ZIP
+      //STATE2
+      if (state2 === "" || !statePattern.test(state)) {
+          isValid = false;
+          $('spanState2').innerHTML = "State Required";
+      } else {
+          $('spanState2').innerHTML = "";
+      }
+      //ZIP
       if (zip === "" || !zipPattern.test(zip)) {
           isValid = false;
           $('spanZip').innerHTML = "Zip Code Required";
       } else {
           $('spanZip').innerHTML = "";
       }
-      // //PHONE
+      //ZIP2
+      if (zip2 === "" || !zipPattern.test(zip)) {
+          isValid = false;
+          $('spanZip2').innerHTML = "Zip Code Required";
+      } else {
+          $('spanZip2').innerHTML = "";
+      }
+      //PHONE
       if (phone === "" || !phonePattern.test(phone)) {
           isValid = false;
           $('spanPhone').innerHTML = "Phone Number Required";
       } else {
           $('spanPhone').innerHTML = "";
       }
-      // //EMAIL
+      //EMAIL
       if (email === "" || !emailPattern.test(email)) {
           isValid = false;
           $('spanEmail').innerHTML = "Valid Email Required";
@@ -122,13 +175,36 @@ window.addEventListener('load', () => {
       } else {
           $('spanEmail').innerHTML = "";
       }
-      // document.getElementById('name').focus();
+      //CVC
+      if (cvc === "" || !cvcPattern.test(cvc)) {
+          isValid = false;
+          $('spanCVC').innerHTML = "Valid CVC Required";
+
+      } else {
+          $('spanCVC').innerHTML = "";
+      }
+
+      //CC
+      if (cc === "" || !visaPattern.test(cc) || !masterPattern.test(cc) || !amexPattern.test(cc)) {
+          isValid = false;
+          $('spanCC').innerHTML = "Valid CC Required";
+      } else if (cc === visaPattern.test(cc)) {
+        $('spanCC').innerHTML = "Visa";
+      }
+      
+      //4444444444444
+
+
+
+
+
   });
   $('name1').focus();
 
 });
 
-/* -------------------------------------------VALIDATE BILLING INPUTS---------------------------------------------- */
+/* --------------------------------VALIDATE BILLING INPUTS------------------------------- */
+//SAME AS DELIVERY INFO
 function addressFunction() {
     if ($("same").checked) {
         $("name2").value = $('name1').value;
@@ -147,3 +223,7 @@ function addressFunction() {
         $("zip2").value = "";
     }
 }
+
+//CREDIT CARD VALIDATION
+
+//CVC CODE
